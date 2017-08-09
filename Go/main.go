f1c28df650b88902354e84933a5135864a3a347e
LAPTOP-DJVQKTC1\rewat <rewat@tyche.co.th>
2017-08-09 10:12:27 +0700

    mini project

---
+++ Go/main.go
@@ -0,0 +1,15 @@
+package main // declare package
+import "fmt" // import fmt
+
+func main(){
+	fmt.Println("Hello world This go - programming By F :)")
+
+	//. Variable etc.
+	var number int = 100
+	number2 := 200.34
+	str := "String"
+	fact := true
+	var arr = [3]string {"x","y","c"}
+
+	fmt.Println("arr ->",arr)
+}
